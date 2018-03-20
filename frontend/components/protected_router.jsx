1cf860fb7317e8165834abb6a6ebbd529023f647
Theodore Summer <TWSummer@gmail.com>
2018-03-20 16:55:46 -0700

    Add react-router package

--- frontend/components/protected_router.jsx
+++ frontend/components/protected_router.jsx
@@ -5,8 +5,14 @@ import React from 'react';
 const ProtectedRouter = (props) => {
   return (
     <div>
+      <SiteHeaderContainer />
       <Switch>
-
+        // <Route exact path="/profile" component={ProfileContainer} />
+        // <Route exact path="/profile/:username" component={ViewProfileContainer} />
+        // <Route exact path="/questions" component={QuestionContainer} />
+        // <Route exact path="/friends" component={FriendSearchContainer} />
+        // <Route exact path="/messages" component={MessageContainer} />
+        // <Redirect from='/' to='/profile'/>
       </Switch>
     </div>
   );
